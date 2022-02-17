c376f2908a35f884b129c13facb3e3b47d6f7efb
Szczepan <szczep.kowalik@gmail.com>
2022-02-17 12:28:18 +0100

    api routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CartProductController;
 
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::post('products', [ProductController::class, 'store']);
 Route::put('products/{id}', [ProductController::class, 'update']);
 Route::delete('products/{id}', [ProductController::class, 'delete']);
+
+Route::post('cart', [CartController::class, 'create']);
+Route::get('cart/{id}/products', [CartProductController::class, 'index']);
+Route::post('cart/{id}/products', [CartProductController::class, 'store']);
